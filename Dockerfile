d302f447ecc655932a6dccec506f20001f0b0a06
Djalma Filho <devdjalma@gmail.com>
2024-12-04 16:28:37 -0300

    Docker

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.8-slim-buster
+
+# Configurar variáveis de ambiente sensíveis
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+# Copiar o arquivo para poder instalar depois
+COPY requirements.txt requirements.txt
+
+# Instalar dependências do sistema
+RUN apt-get update && apt-get install -y libpq-dev gcc
+
+# Instalar Requirements
+RUN pip3 install -r requirements.txt 
+
+COPY . .
+
+CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&9v$(crh48u=b11xph9=dr2st2u9h1$s1)4dza*2co4k$fd8m1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 
 # Application definition
